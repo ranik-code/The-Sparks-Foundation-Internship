67adf37a62dd325e1a16b99130deca635dab7c7e
Rani Kumrawat <76204310+ranik-code@users.noreply.github.com>
2021-08-19 21:17:51 +0530

    Update BankSparks.sql

--- Task1/Sql/BankSparks.sql
+++ Task1/Sql/BankSparks.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sparks_bank`
+-- Database: `Sparks_bank`
 --
 
 -- --------------------------------------------------------
