b1106bfbb06fefaef1368549aa4326a7938fbcf6
Rani Kumrawat <76204310+ranik-code@users.noreply.github.com>
2021-08-21 00:48:10 +0530

    Update Index.php

--- Task1/Index.php
+++ Task1/Index.php
@@ -24,31 +24,34 @@
               <div class="col-sm-12 col-md">
                 <div class="heading text-center my-5"><br><br><br>
                   <h3>Welcome to</h3>
-                  <h1>SPARKS BANK</h1>
+                  <h1><B>SPARKS BANK</B></h1>
+                  <h4>Here for you..</h4>
                 </div>
               </div>
-              <div class="col-sm-12 col-md img text-center">
-                <img src="Images/bank-p.png" class="img-fluid pt-2" height="50%" width="50%">
+              <div class="col-sm-12 col-md img text-center"> 
+                <img src="Images/bank-p.png" class="img-fluid pt-2" height="50%" width="50%"> 
               </div>
+              <!-- <div class="column image"><img src="images/bank4.jpg"  /></div> -->
             </div>
 
       <!-- Activity section -->
             <div class="row activity text-center">
                   <div class="col-md act">
-                    <img src="Images/1-p.png" class="img-fluid" height="70%" width="70%">
+                    <img src="Images/user2.png" class="img-fluid" height="90%" width="90%">
                     <br>
                     <a href="CreateUser.php"><button>Create a User</button></a>
                   </div>
                   <div class="col-md act">
-                    <img src="Images/rk-p.png" class="img-fluid" height="70%" width="70%">
+                    <img src="Images/rk-p.png" class="img-fluid" height="50%" width="50%">
                     <br>
                     <a href="TransferMoney.php"><button>Make a Transaction</button></a>
                   </div>
                   <div class="col-md act">
-                    <img src="Images/trans1.png" class="img-fluid" height="80%" width="70%">
+                    <img src="Images/trans1.png" class="img-fluid" height="50%" width="50%">
                     <br>
                     <a href="TransactionHistory.php"><button>Transaction History</button></a>
                   </div>
+            </div>
             </div>
       </div>
       <footer class="text-center mt-5 py-2">
@@ -58,3 +61,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   </body>
 </html>
+
